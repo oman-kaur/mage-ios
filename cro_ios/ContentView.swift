2255a3174adc25595b93347d9046cd2f4f49dd5b
Gagan Suie <gagan.suie@codecrow.io>
2022-07-09 00:35:33 -0500

    Initial Commit

---
+++ cro_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cro_ios
+//
+//  Created by Gagan Suie on 7/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cro_ios/cro_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cro_iosApp.swift
+//  cro_ios
+//
+//  Created by Gagan Suie on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct cro_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
